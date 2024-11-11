b4019e5f47e8f4bf4b93e996ebbbd34279e96ae0
eshkere007 <cvkkas@gmail.com>
2024-11-11 09:54:39 +0700

    Add files via upload

---
+++ SingletonPattern/lab2/lab2-1.cs
@@ -0,0 +1,29 @@
+﻿public class Servers
+{
+    private static readonly Lazy<Servers> instance = new Lazy<Servers>(() => new Servers());
+    private readonly HashSet<string> _serverList;
+    private Servers()
+    {
+        _serverList = new HashSet<string>();
+    }
+    public static Servers GetInstance() => instance.Value;
+
+    public bool AddServer(string server)
+    {
+        if (!server.StartsWith("http://") && !server.StartsWith("https://"))
+        {
+            return false;
+        }
+        return _serverList.Add(server);
+    }
+
+    public List<string> GetHttp()
+    {
+        return new List<string>(_serverList.Where(s => s.StartsWith("http://", StringComparison.OrdinalIgnoreCase)));
+    }
+
+    public List<string> GetHttps()
+    {
+        return new List<string>(_serverList.Where(s => s.StartsWith("https://", StringComparison.OrdinalIgnoreCase)));
+    }
+}
---
+++ SingletonPattern/lab2/lab2-2.cs
@@ -0,0 +1,42 @@
+﻿public class Servers2
+{
+    private static readonly Servers2 instance = new Servers2();
+    private readonly HashSet<string> _serverList;
+    private readonly object _lock = new object();
+
+    private Servers2()
+    {
+        _serverList = new HashSet<string>();
+    }
+
+    public static Servers2 GetInstance() => instance;
+
+    public bool AddServer(string server)
+    {
+        if (!server.StartsWith("http://") && !server.StartsWith("https://"))
+        {
+            return false;
+        }
+
+        lock (_lock)
+        {
+            return _serverList.Add(server);
+        }
+    }
+
+    public List<string> GetHttp()
+    {
+        lock (_lock)
+        {
+            return new List<string>(_serverList.Where(s => s.StartsWith("http://", StringComparison.OrdinalIgnoreCase)));
+        }
+    }
+
+    public List<string> GetHttps()
+    {
+        lock (_lock)
+        {
+            return new List<string>(_serverList.Where(s => s.StartsWith("https://", StringComparison.OrdinalIgnoreCase)));
+        }
+    }
+}
