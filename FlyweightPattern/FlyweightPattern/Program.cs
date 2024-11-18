a906e605c37d7a4a02c516a294b217ffd072e790
eshkere007 <cvkkas@gmail.com>
2024-11-18 10:02:25 +0700

    Add files via upload

---
+++ FlyweightPattern/FlyweightPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using FlyweightPattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CharacterFactory factory = new CharacterFactory();
+
+        Character char1 = factory.GetCharacter("Лала", "Воин", "warrior_image.png");
+        Character char2 = factory.GetCharacter("Лала", "Маг", "mage_image.png");
+        Character char3 = factory.GetCharacter("Лала", "Воин", "warrior_image.png");
+
+        char1.Display();
+        char2.Display();
+        char3.Display();
+
+        Console.WriteLine("char1 == char3: " + (char1 == char3));
+
+        CharacterAttributes char1Attributes = new CharacterAttributes(10, 150);
+        CharacterAttributes char2Attributes = new CharacterAttributes(12, 200);
+
+        Console.WriteLine("Лала-воин:");
+        char1Attributes.DisplayAttributes();
+
+        Console.WriteLine("Лала-маг:");
+        char2Attributes.DisplayAttributes();
+    }
+}
