0265e4df4a6edcffcd6ca2947380c330dfdb8d76
eshkere007 <cvkkas@gmail.com>
2024-12-09 10:57:46 +0700

    Add files via upload

---
+++ IteratorPattern/IteratorPattern/Library.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorPattern
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> _books = new List<Book>();
+        public void AddBook(Book book)
+        {
+            _books.Add(book);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return _books.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ IteratorPattern/IteratorPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using IteratorPattern;
+
+public class Program
+{
+    public static void Main()
+    {
+        Library library = new Library();
+
+        library.AddBook(new Book("Война и мир", "Лев Толстой", 1869));
+        library.AddBook(new Book("Евгений Онегин", "Александр Пушкин", 1831));
+        library.AddBook(new Book("Приключения Алисы в Стране чудес", "Льюис Кэрролл", 1865));
+
+        foreach (var book in library) Console.WriteLine(book);
+    }
+}
